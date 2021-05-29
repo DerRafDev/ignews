1c53750f268d044b73f214a6ba50921817783c8f
DerRafDev <rafael.sordi.lino@gmail.com>
2021-05-29 17:23:06 -0400

    Test Posts  Page

---
+++ src/tests/pages/Posts.spec.tsx
@@ -0,0 +1,63 @@
+import { render, screen } from '@testing-library/react'
+import { mocked } from 'ts-jest/utils'
+
+import { stripe } from '../../services/stripe';
+import Posts, { getStaticProps } from '../../pages/posts'
+import { getPrismicClient } from '../../services/prismic'
+
+const posts = [
+    { 
+        slug: 'my-new-post',
+        title: 'My new Post',
+        excerpt: 'Post excerpt',
+        updatedAt: 'March, 10'
+    }
+];
+
+jest.mock('../../services/prismic')
+
+describe('Posts page', () => {
+    it('renders correctly', () => {
+        render(<Posts posts={posts} />)
+
+        expect(screen.getByText("My new Post")).toBeInTheDocument()
+    });
+
+    it('loads initial data', async () => {
+        const getPrismicClientMocked = mocked(getPrismicClient)
+
+        getPrismicClientMocked.mockReturnValueOnce({
+            query: jest.fn().mockResolvedValueOnce({
+                results: [
+                    {
+                        uid: 'my-new-post',
+                        data: {
+                            title: [
+                                {type: 'heading', text: 'My new Post' }
+                            ],
+                            content: [
+                                { type: 'paragraph', text: 'Post excerpt' }
+                            ],
+                        },
+                        last_publication_date: '04-01-2021',
+                    }
+                ]
+            })
+        } as any)
+
+        const response = await getStaticProps({})
+
+        expect(response).toEqual(
+            expect.objectContaining({
+                props: {
+                    posts: [{
+                        slug: 'my-new-post',
+                        title: 'My new Post',
+                        excerpt: 'Post excerpt',
+                        updatedAt: 'April 01, 2021'
+                    }]
+                }
+            })
+        )
+    });
+})
